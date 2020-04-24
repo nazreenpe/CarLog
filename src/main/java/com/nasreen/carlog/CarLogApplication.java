0b6555506839771599ac7ab1d871784c719696c4
Nasreen Habeeb Ur Rahiman <nazreenpe@gmail.com>
2020-04-24 12:12:49 -0700

    Initial commit

---
+++ src/main/java/com/nasreen/carlog/CarLogApplication.java
@@ -0,0 +1,13 @@
+package com.nasreen.carlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarLogApplication.class, args);
+	}
+
+}
