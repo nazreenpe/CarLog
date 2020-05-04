15a1baab60633b68f031a17111cbc02b65e75f73
Nasreen Habeeb Ur Rahiman <nazreenpe@gmail.com>
2020-05-04 15:28:24 -0700

    Implement get Activity from db

--- src/main/java/com/nasreen/carlog/db/ActivityRepository.java
+++ src/main/java/com/nasreen/carlog/db/ActivityRepository.java
@@ -1,15 +1,21 @@
 package com.nasreen.carlog.db;
 
 import com.nasreen.carlog.model.Activity;
+import com.nasreen.carlog.model.ActivityType;
 import org.jdbi.v3.core.Jdbi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
+import java.util.Optional;
+import java.util.UUID;
 
 @Repository
 @Transactional
 public class ActivityRepository {
+    public static final String SELECT_QUERY = "SELECT * FROM activities WHERE record_id = :recordId AND id = :id";
+    public static final String INSERT_QUERY = "INSERT INTO activities(id, type, record_id)" +
+            "  VALUES(:id, :type, :recordId)";
     private Jdbi jdbi;
 
     @Autowired
@@ -19,13 +25,22 @@ public class ActivityRepository {
 
     public Activity save(Activity activity) {
         return jdbi.withHandle(handle -> {
-            handle.createUpdate("INSERT INTO activities(id, type, record_id)" +
-                    "  VALUES(:id, :type, :record_id)")
-                    .bind("id", activity.getId())
+            handle.createUpdate(INSERT_QUERY)
+                    .bind("id", activity.getId().toString())
                     .bind("type", activity.getType())
-                    .bind("record_id", activity.getRecordId())
+                    .bind("recordId", activity.getRecordId().toString())
                     .execute();
             return activity;
         });
+    }
+
+    public Optional<Activity> findById(UUID recordId, UUID id) {
+        return jdbi.withHandle(handle -> handle.createQuery(SELECT_QUERY)
+                .bind("recordId", recordId.toString())
+                .bind("id", id.toString())
+                .map((rs, ctx) -> new Activity(UUID.fromString(rs.getString("id")),
+                        ActivityType.valueOf(rs.getString("type")),
+                        UUID.fromString(rs.getString("record_id"))))
+                .findFirst());
     }
 }
