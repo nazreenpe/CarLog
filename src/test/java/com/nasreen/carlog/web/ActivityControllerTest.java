34998c3599299b3ba99c1ece4c004122a421aa69
Nasreen Habeeb Ur Rahiman <nazreenpe@gmail.com>
2020-05-05 07:09:42 -0700

    Delete activity from db, added

--- src/test/java/com/nasreen/carlog/web/ActivityControllerTest.java
+++ src/test/java/com/nasreen/carlog/web/ActivityControllerTest.java
@@ -25,7 +25,6 @@ import java.util.Map;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
