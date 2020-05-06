0f28f1f17070069aa6d90fe28a617717fe3ec20c
Nasreen Habeeb Ur Rahiman <nazreenpe@gmail.com>
2020-05-06 12:56:03 -0700

    Add skeleton forms for Activity, Record with dropdwon menu and time picker setup.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import './App.css';
 import CarForm from './components/CarForm';
+import Record from './components/Record';
+import "react-datepicker/dist/react-datepicker.css";
 import CarDetails from './components/CarDetails';
 import Header from './components/Header';
+import Activity from './components/Activity';
 
 function App() {
   return (
@@ -15,7 +18,9 @@ function App() {
         <p>
           Welcome to CarLog
         </p>
-        {/* <CarForm startValue="No cars Yet!"></CarForm> */}
+        <CarForm startValue="No cars Yet!"></CarForm>
+        <Record/>
+        <Activity/>
         {/* <CarDetails id="7bbd93a2-40b4-4ca6-bb47-7d6955e814b8"></CarDetails> */}
       </div>
     </div>
