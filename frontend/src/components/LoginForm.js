ce5f4c536d3c99df8e542a9a88451f34321b2921
Nasreen Habeeb Ur Rahiman <nazreenpe@gmail.com>
2020-05-10 11:19:38 -0700

    Add LoginForm

--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -2,33 +2,72 @@ import React from 'react'
 import { Button, Form, Grid, Header, Image, Message, Segment, Label } from 'semantic-ui-react'
 import { NavLink } from 'react-router-dom'
 
-const LoginForm = () => (
-  <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
-    <Grid.Column style={{ maxWidth: 450 }}>
-      <Header as='h2' color='teal' textAlign='center'>
-        <Image src='/favicon.png' /> Log-in to your account
-      </Header>
-      <Form size='large'>
-        <Segment stacked>
-          <Form.Input fluid icon='user' iconPosition='left' placeholder='E-mail address' />
-          <Form.Input
-            fluid
-            icon='lock'
-            iconPosition='left'
-            placeholder='Password'
-            type='password'
-          />
+class LoginForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasSubmited: false,
+      email: null,
+      password: null
+    }
+  }
 
-          <Button color='teal' fluid size='large'>
-            Login
+  doLogin = (e, value) => {
+    console.log(this.state);
+    fetch("/api/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      }
+    })
+    .then(res => res.json())
+    .then(res => console.log(res));
+  }
+
+  handleEmail = (e, input) => {
+    this.setState({email: input.value})
+  }
+
+  handlePassword = (e, input) => {
+    this.setState({password: input.value})
+  }
+
+  render() {
+    return (
+      <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
+        <Grid.Column style={{ maxWidth: 450 }}>
+          <Header as='h2' color='teal' textAlign='center'>
+            <Image src='/favicon.png' /> Log-in to your account
+          </Header>
+          <Form size='large' onSubmit={this.doLogin}>
+            <Segment stacked>
+              <Form.Input fluid icon='mail'
+                iconPosition='left'
+                placeholder='E-mail address' 
+                onChange={this.handleEmail}
+                />
+              <Form.Input
+                fluid
+                icon='lock'
+                iconPosition='left'
+                placeholder='Password'
+                type='password'
+                onChange={this.handlePassword}
+              />
+
+              <Button color='teal' fluid size='large'>
+                Login
           </Button>
-        </Segment>
-      </Form>
-      <Message>
-        New to us? <Label as={NavLink} to="/signup">Sign Up</Label>
-      </Message>
-    </Grid.Column>
-  </Grid>
-)
+            </Segment>
+          </Form>
+          <Message>
+            New to us? <Label as={NavLink} to="/signup">Sign Up</Label>
+          </Message>
+        </Grid.Column>
+      </Grid>
+    )
+  }
+}
 
 export default LoginForm
