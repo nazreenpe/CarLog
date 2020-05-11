00b1ae2b49509f3d80f7c53934e7f3787b21e583
Nasreen Habeeb Ur Rahiman <nazreenpe@gmail.com>
2020-05-11 09:52:21 -0700

    Cleaned up dashboard menu

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,5 +1,18 @@
 import React, {Component} from 'react'
 import { NavLink, Redirect } from 'react-router-dom'
+import {
+Button,
+Container,
+Dropdown,
+Form,
+Grid,
+Header,
+Image,
+Menu,
+Message,
+Segment,
+Label } from 'semantic-ui-react'
+
 
 class Dashboard extends Component {
   constructor(props) {
@@ -7,11 +20,29 @@ class Dashboard extends Component {
   }
 
   render() {
-    if (!localStorage.getItem("currentUser")) {
+    let userJson = localStorage.getItem("currentUser")
+    if (!userJson) {
       return <Redirect to="/login" />
     }
+    let currentUser = JSON.parse(userJson)
     return (
-      <div>This is where the dashboard goes</div>
+      <Menu fixed='top' inverted>
+        <Container>
+          <Menu.Item as='a' header>
+            <Image size='mini' src='/favicon.png' style={{ marginRight: '1.5em' }} />
+            CarLog
+          </Menu.Item>
+          <Menu.Item as={NavLink} to="/dashboard">Dashboard</Menu.Item>
+
+          <Dropdown item simple text={currentUser.name}>
+            <Dropdown.Menu>
+              <Dropdown.Item as={NavLink} to="/profile">Profile</Dropdown.Item>
+              <Dropdown.Divider />
+              <Dropdown.Item as={NavLink} to="/logout">Log out</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </Container>
+      </Menu>
     )
   }
 }
