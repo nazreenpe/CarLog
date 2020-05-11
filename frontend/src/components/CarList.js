6268cfcad57e5e5e94afcd1434322dce11ffbcaa
Nasreen Habeeb Ur Rahiman <nazreenpe@gmail.com>
2020-05-11 11:29:09 -0700

    List all cars and render as cards

--- frontend/src/components/CarList.js
+++ frontend/src/components/CarList.js
@@ -1,8 +1,43 @@
 import React, {Component} from 'react'
+import {Card} from 'semantic-ui-react'
 
 class CarList extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      cars: []
+    }
+  }
+
+  componentDidMount() {
+    fetch("/api/cars", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      }
+    })
+    .then(res => {
+        if (!res.ok) {
+          throw new Error()
+        }
+        return res.json()
+    })
+    .then(cars => {
+      this.setState({cars: cars})
+      console.log(cars)
+    })
+  }
+
   render() {
-    return <div> Work in progress: List of cars go here </div>
+    return <div>
+      {this.state.cars.map(car => (<Card
+        image="/favicon.png"
+        header={car.year + " " + car.make + " " + car.model + " " + car.trim}
+        meta={"Vin number=Todo"}
+
+        />))}
+    </div>
   }
 }
 
