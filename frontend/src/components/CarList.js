74ddf8731788a10fcabdab79b25e168fba2c1299
Nasreen Habeeb Ur Rahiman <nazreenpe@gmail.com>
2020-05-11 10:29:30 -0700

    Revert "WIP"
    This reverts commit 4827fa1c8134beba56bd26811af3e7a25933678b.

--- frontend/src/components/CarList.js
+++
@@ -1,9 +0,0 @@
-import React, {Component} from 'react'
-
-class CarList extends Component {
-  render() {
-    return <div> Work in progress: List of cars go here </div>
-  }
-}
-
-export default CarList
--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import { NavLink, Redirect, HashRouter, Route } from 'react-router-dom'
+import { NavLink, Redirect } from 'react-router-dom'
 import {
 Button,
 Container,
@@ -8,14 +8,11 @@ Form,
 Grid,
 Header,
 Image,
-List,
-Divider,
 Menu,
 Message,
 Segment,
 Label } from 'semantic-ui-react'
 
-import CarList from './CarList.js'
 
 class Dashboard extends Component {
   constructor(props) {
@@ -29,30 +26,23 @@ class Dashboard extends Component {
     }
     let currentUser = JSON.parse(userJson)
     return (
-      <div>
-        <Menu fixed='top' inverted>
-          <Container>
-            <Menu.Item as='a' header>
-              <Image size='mini' src='/favicon.png' style={{ marginRight: '1.5em' }} />
-              CarLog
-            </Menu.Item>
-            <Menu.Item as={NavLink} to="/dashboard">Dashboard</Menu.Item>
+      <Menu fixed='top' inverted>
+        <Container>
+          <Menu.Item as='a' header>
+            <Image size='mini' src='/favicon.png' style={{ marginRight: '1.5em' }} />
+            CarLog
+          </Menu.Item>
+          <Menu.Item as={NavLink} to="/dashboard">Dashboard</Menu.Item>
 
-            <Dropdown item simple text={currentUser.name}>
-              <Dropdown.Menu>
-                <Dropdown.Item as={NavLink} to="/profile">Profile</Dropdown.Item>
-                <Dropdown.Divider />
-                <Dropdown.Item as={NavLink} to="/logout">Log out</Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
-          </Container>
-        </Menu>
-        <Container style={{ marginTop: '7em' }}>
-          <HashRouter>
-            <Route exact path="/dashboard" component={CarList} />
-          </HashRouter>
+          <Dropdown item simple text={currentUser.name}>
+            <Dropdown.Menu>
+              <Dropdown.Item as={NavLink} to="/profile">Profile</Dropdown.Item>
+              <Dropdown.Divider />
+              <Dropdown.Item as={NavLink} to="/logout">Log out</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
         </Container>
-      </div>
+      </Menu>
     )
   }
 }
